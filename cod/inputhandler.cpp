7ce0f666a443998f6dc2adf24025ebce22805831
Matz1993 <muehlbauer-matthias@web.de>
2016-11-25 09:45:44 +0100

    inputhandler.cpp & inputhandler.h add

---
+++ cod/inputhandler.cpp
@@ -0,0 +1,58 @@
+#include "inputhandler.h"
+
+InputHandler::InputHandler()
+{
+
+}
+
+void execute()
+{
+    servo *s = new servo();
+    engine *e = new engine();
+    unsigned value = 62;
+    int c;
+    int count = 0;
+    bool dir = true;
+
+    system("/bin/stty raw");
+    while((c=getchar())!= '.'){
+        system("clear");
+        //putchar(c);
+        if(c == '1' && count > 0)
+            count = count - 10;
+        else if ( c == '2' && count < 100)
+            count = count + 10;
+        else if(c == '7'){
+            if(dir == true)
+                dir = false;
+            else
+                dir = true;
+            e->setDir(dir);
+        }
+        else if(c == '6' && value > 52)
+        {
+            value--;
+            value = 52;
+            s->setPWMSignal(value);
+        }
+        else if(c == '4' && value < 75)
+        {
+            value++;
+            value = 75;
+            s->setPWMSignal(value);
+        }
+        else if(c == '5')
+        {
+
+            value++;
+            value = 61;
+            s->setPWMSignal(value);
+        }
+        e->writeDirection(dir);
+        cout << "\n Power: ";
+        cout << count << endl;
+        cout << " \n";
+        e->setPWMSignal(count);
+    }
+    system("/bin/stty cooked");
+}
---
+++ cod/inputhandler.h
@@ -0,0 +1,12 @@
+#ifndef INPUTHANDLER_H
+#define INPUTHANDLER_H
+
+
+class InputHandler
+{
+public:
+    InputHandler();
+    void execute();
+};
+
+#endif // INPUTHANDLER_H
